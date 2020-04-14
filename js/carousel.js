72c0a642f9670c6e2d9bdbd4aa7b8453d8ed02e5
ralfferreira <ralfferreira995@gmail.com>
2020-04-14 18:53:57 -0300

    Atualizando carrossel

--- js/carousel.js
+++ js/carousel.js
@@ -1,14 +1,26 @@
-var corpo = 0;
-		  carousel();
-		  
-		  function carousel() {
-			var i;
-			var x = document.getElementsByClassName("fotos-carousel");
-			for (i = 0; i < x.length; i++) {
-			  x[i].style.display = "none";  
-			}
-			corpo++;
-			if (corpo > x.length) {corpo = 1}    
-			x[corpo-1].style.display = "block";  
-			setTimeout(carousel, 5000); 
-		  }
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" btnactive", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " btnactive";
+}
